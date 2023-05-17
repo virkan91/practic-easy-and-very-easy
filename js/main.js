d0e2909863e25498e4ce72820d9b60d2fbd3de5b
virkann <virkan90@gmail.com>
2023-05-17 07:04:47 +0300

    modified readmi and main.js

--- js/main.js
+++ js/main.js
@@ -60,18 +60,16 @@
 // 5. Создайте функцию, которая ищет индекс данного элемента в массиве. 
 //Если элемент присутствует, он должен вернуть индекс, в противном случае он должен вернуть -1.
 
-
-
-    function search(arr, item) {
-     for (let i = 0; i < arr.length; i++) {
-        if (arr[i] == item) {
-        return i;
-        }
-     }
-    return -1;
-    }
-    console.log(search([1, 2, 3, 4], 3));
-    console.log(search([2, 4, 6, 8, 10], 8));
+    // function search(arr, item) {
+    //  for (let i = 0; i < arr.length; i++) {
+    //     if (arr[i] == item) {
+    //     return i;
+    //     }
+    //  }
+    // return -1;
+    // }
+    // console.log(search([1, 2, 3, 4], 3));
+    // console.log(search([2, 4, 6, 8, 10], 8));
 
 
 
@@ -168,3 +166,121 @@
     // 	}
     // 	return false;
     // }
+
+
+//__________________________________________________________________________________________________________________________________///
+
+
+
+// 17-05-20023//
+
+
+// VERY-EASY
+
+
+// 1. Сложите коробки
+
+    // function stackBoxes(n) {
+    // 	return n * n;
+    // }
+    // console.log(stackBoxes(1));
+
+
+// 2 Преобразование массива в строку
+
+    // function arrayToString(arr) {
+    // 	let  str = arr.join("");
+    // 	return str;
+    // }
+
+
+// 3.Объединение двух целочисленных массивов
+
+ // spread
+// function concat(arr1, arr2) {
+// 	let all = [...arr1, ...arr2];  
+// 	return all;
+	
+//}
+// concat
+// function concat(arr1, arr2) {
+// 	let newArray = arr1.concat(arr2);
+// 	return newArray;
+// }
+
+// console.log(concat([1, 3, 5], [2, 6, 8]))
+
+
+// 4. Проверить, содержит ли массив заданное число
+
+// function check(arr, el) {
+// 	return arr.includes(el);
+// }
+// console.log(check([1, 2, 3, 4, 5], 3));
+
+
+// 5.Минимум II: логическая избыточность
+
+// function parity(n) {
+// 	let remainder = Boolean(n % 2)
+// 	if(remainder === false) {
+// 		return "even"
+// 	}
+// 	if(remainder === true) {
+// 		return "odd"
+// 	}
+// }
+
+
+// 6 Количество наклеек
+
+// // function howManyStickers(n) {
+// // 	return (n * n)* 6;
+// // }
+// console.log(howManyStickers(1));
+
+// 7.Четыре пассажира и водитель
+
+// function carsNeeded(n) {
+// 	return Math.ceil(n / 5);
+// }
+// console.log(carsNeeded(5));
+
+
+
+// 8.Проверить строку на наличие пробелов
+
+// function hasSpaces(str) {
+// 	return str.includes(" ")
+// }
+// console.log(hasSpaces("hello"));
+// console.log(hasSpaces("hello, world"));
+
+
+// 9 Объем коробки
+
+// function volumeOfBox(sizes) {
+// return sizes.width * sizes.length * sizes.height;
+// }
+// console.log(volumeOfBox({ width: 2, length: 5, height: 1 }));
+
+
+// 10 Воссоздание функции abs()
+
+// function absolute(n) {
+// 	if(n < 0 ){
+// 		return 0 - n;
+// 	}
+// 	return n;
+// }
+
+// 11
+
+function isLastCharacterN(word) {
+	if(word.endsWith("n")){
+		return true;
+	}
+	return false;
+}
+console.log(isLastCharacterN("Aiden"));
+
